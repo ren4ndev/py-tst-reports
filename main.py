e7865bf90c3441145bc6861d101aff94d46c4b62
Renan Oliveira <oliveira.renan.rg@gmail.com>
2023-02-21 19:40:33 -0300

    refactor: Trata exceções e diretórios

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ class MainScript:
 
     def _make_sheets_directory(self):
         '''Cria diretório para armazenar planilhas'''
-        os.mkdir(self.sheets_path)
+        if not os.path.exists(self.sheets_path):
+            os.mkdir(self.sheets_path)
 
     def _clear_temporary_files(self):
         """
@@ -29,30 +30,34 @@ class MainScript:
             Responsável por executar todos os métodos do script
             de acordo com os requisitos do projeto
         """
-        buffer = io.StringIO()
-        sys.stdout = buffer
-        sys.stderr = buffer
+        try:
+            buffer = io.StringIO()
+            sys.stdout = buffer
+            sys.stderr = buffer
 
-        (week_start, week_end) = Dates.get_days_of_the_week()
-        print(week_start)
-        print(week_end)
+            (week_start, week_end) = Dates.get_days_of_the_week()
 
-        scraper = NotebookScraper(
-            pdf_directory=self.pdf_path,
-            start_date='13/02/2023',
-            end_date='19/02/2023'
-        )
-        scraper.execute()
-        (extracted_data, duplicates) = ProcessExtraction(
-            pdf_directory=self.pdf_path
-        ).execute()
-        self._make_sheets_directory()
-        SheetsConverter(
-            sheets_directory=self.sheets_path,
-            extracted_data=extracted_data,
-            duplicates=duplicates
-        ).execute()
-        self._clear_temporary_files()
+            scraper = NotebookScraper(
+                pdf_directory=self.pdf_path,
+                start_date='13/02/2023',  # Data fixada apenas para teste
+                end_date='19/02/2023'  # Data fixada apenas para teste
+            )
+            scraper.execute()
+
+            (extracted_data, duplicates) = ProcessExtraction(
+                pdf_directory=self.pdf_path
+            ).execute()
+
+            self._make_sheets_directory()
+            SheetsConverter(
+                sheets_directory=self.sheets_path,
+                extracted_data=extracted_data,
+                duplicates=duplicates
+            ).execute()
+        except Exception as err:
+            print('Error: {}'.format(err))
+        finally:
+            self._clear_temporary_files()
 
 
 if __name__ == '__main__':
